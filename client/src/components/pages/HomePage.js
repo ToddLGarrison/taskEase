43050d4870eda03822e1e1032a1ce32c95c6b747
Todd Garrison <toddlgarrison@gmail.com>
2023-09-27 14:36:39 -0400

    Add homepage

---
+++ client/src/components/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage = (props) => {
+    return (
+        <div className="primary home-box">
+            <div className="grid homepage">
+                <h2 className="homepage-title">Conquer your todo's with TaskEase</h2>
+                <p className="login-call">Please login to manage your lists</p>
+                <p className="developed-by-text">Developed by Todd Garrison</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default HomePage
