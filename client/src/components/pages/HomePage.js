c8b72b5e8f304806c9d7da192e3be86f295607ec
Todd Garrison <toddlgarrison@gmail.com>
2023-09-28 15:42:29 -0400

    Update user name css

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 const HomePage = (props) => {
+
     return (
         <div className="primary home-box">
             <div className="grid homepage">
                 <h2 className="homepage-title">Conquer your todo's with TaskEase</h2>
-                <p className="login-call">Please <a href="/user-sessions/new">login</a> to manage your lists</p>
+                <div>
+                    {props.user ?(
+                        <div>
+                            <a href="/lists/new" className="create-list-call">Create a new list</a>
+                            <a href="/lists" className="go-to-list-call">See your lists</a>ß
+                        </div>
+                    ) : (
+                        <div>
+                            <p className="login-call">Please <a href="/user-sessions/new">login</a> to manage your lists</p>
+                        </div>
+                    ) }
+                </div>
                 <p className="developed-by-text">Developed by Todd Garrison</p>
             </div>
 
