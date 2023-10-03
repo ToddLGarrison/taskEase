5083e5759b2aa3dcb170fb583602f77d3dd98a3c
Todd Garrison <toddlgarrison@gmail.com>
2023-10-03 16:11:15 -0400

    Add taskForm to ToDoListShowPage

--- client/src/components/pages/ToDoListShowPage.js
+++ client/src/components/pages/ToDoListShowPage.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react"
+import TaskForm from "../tasks/TaskForm"
+import TaskList from "../tasks/TaskList"
+import translateServerErrors from "../../services/translateServerErrors"
+import { Redirect } from "react-router-dom"
 
 const ToDoListShowPage = (props) => {
     const [toDoList, setToDoList] = useState({
@@ -6,6 +10,40 @@ const ToDoListShowPage = (props) => {
         description: "",
         tasks: []
     })
+
+    const [errors, setErrors] = useState([])
+    const [tasks, setTasks] = useState([])
+    const [shouldRedirect, setShouldRedirect] = useState(false)
+
+    const postTask = async (newTask) => {
+        try {
+            const toDoListId = props.match.params.id
+            const response = await fetch(`/api/v1/Lists/${toDoListId}/tasks`, {
+                method: "Post",
+                headers: new Headers({
+                    "Content-Type": "application/json"
+            }),
+            body: JSON.stringify(newTask)
+        })
+            if(!response.ok) {
+                if(response.status === 422) {
+                    const errorBody = await response.json()
+                    const newErrors = translateServerErrors(errorBody.errors.data)
+                    return setErrors(newErrors)
+                } else {
+                    const errorMessage = `${response.status} (${response.statusText})`
+                    const error = new Error(errorMessage)
+                    throw(error)
+                }
+            } else {
+                const body = await response.json()
+                setErrors([])
+                return setTasks([...tasks, body.task])
+            }
+        } catch (error) {
+            console.error(`Error in fetch ${error.message}`)
+        }
+    }
 
     const getToDoList = async () => {
         try {
@@ -28,11 +66,32 @@ const ToDoListShowPage = (props) => {
         getToDoList()
     }, [])
 
+    let toDoListDescriptionSection
+    if(toDoList.description) {
+        toDoListDescriptionSection = <div className="to-do-list-form-description">
+            <h3>{toDoList.description}</h3>
+        </div>
+    }
+
+    let taskForm
+    if (props.user) {
+        taskForm = (
+            <TaskForm
+                postTask = {postTask}
+                errors={errors} 
+            />
+        )
+    }
+
     return (
         <div className="to-do-list-box">
             <div className="to-do-list-header-box">
                 <h2 className="to-do-list-form-title">{toDoList.name}</h2>
-                <h3 className="to-do-list-form-description">{toDoList.description}</h3>
+                {toDoListDescriptionSection}
+            </div>
+            <div className="to-do-list-task-box">
+                <TaskList tasks={tasks} />
+                    {taskForm}
             </div>
         </div>
     )
