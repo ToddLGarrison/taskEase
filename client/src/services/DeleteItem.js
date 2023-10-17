459a5973bd6c1ec1c3915cdbd4bcba45963f11ed
Todd Garrison <toddlgarrison@gmail.com>
2023-10-17 15:08:04 -0400

    Create DeleteItem

---
+++ client/src/services/DeleteItem.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import ErrorList from '../components/layout/ErrorList';
+import translateServerErrors from './translateServerErrors';
+import { Redirect } from 'react-router-dom';
+
+
+const DeleteItem = props => {
+
+    return (
+        <div>
+        </div>
+    )
+
+};
+
+export default DeleteItem
