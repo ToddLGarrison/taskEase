1acf188f2af9ced1f9212ad05beb96a1287f5376
Todd Garrison <toddlgarrison@gmail.com>
2023-10-17 16:51:42 -0400

    Add post to taskRouter

--- server/src/routes/api/v1/taskRouter.js
+++ server/src/routes/api/v1/taskRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import objection from 'objection'
 import { ValidationError } from 'objection';
 import { Task } from '../../../models/index.js';
+import TaskSerializer from '../../../serializers/TaskSerializer.js';
 import cleanUserInput from '../../../services/cleanUserInput';
 
 const taskRouter = new express.Router({ mergeParams: true });
@@ -31,6 +32,22 @@ taskRouter.get('/:id', async (req, res) => {
     }
 })
 
+taskRouter.post('/', async (req, res) => {
+    const body = cleanUserInput(req.body)
+    const toDoListIdParams = re.params.id
+    const user = req.user
+    const taskDataWithId = {...body, toDoListId: toDoListIdParams, userId: user.id}
 
+    try {
+        const newTask = await Task.query().insertAndFetch(taskDataWithId)
+        const taskSerialized = TaskSerializer.taskDetails(newTask)
+        return res.status(201).json({ task: taskSerialized })
+    } catch (error) {
+        if (error instanceof ValidationError) {
+            return res.status(422).json({ errors: error })
+        }
+        return res.status(500).json({ errors: error })
+    }
+})
 
 export default taskRouter;
