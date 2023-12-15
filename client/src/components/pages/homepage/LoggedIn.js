7177b65f1fed74efb42480902ce8393edb3d0f5d
Todd Garrison <toddlgarrison@gmail.com>
2023-12-15 15:24:33 -0500

    update classNames

--- client/src/components/pages/homepage/LoggedIn.js
+++ client/src/components/pages/homepage/LoggedIn.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const LoggedIn = () => {
     return (
-        <div>
-            <a href="/lists/new" className="create-list-call">Create a new list</a>
-            <a href="/lists" className="go-to-list-call">See your lists</a>
-        </div>
+        <>
+            <div className="create-list-call">
+                <a href="/lists/new">Create a new list</a>
+            </div>
+            <div  className="go-to-list-call">
+                <a href="/lists">See your lists</a>
+            </div>
+        </>
     )
 }
 
