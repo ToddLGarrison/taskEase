b22cf4f6ab83acdb40ebcd670a385ec161761986
Todd Garrison <toddlgarrison@gmail.com>
2023-12-19 15:24:30 -0500

    create handleClick function

--- client/src/components/tasks/TaskItem.js
+++ client/src/components/tasks/TaskItem.js
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 
 const TaskItem = ({ task }) => {
     const [isDone, setIsDone] = useState(false)
-    
+
+    function handleClick() {
+        setIsDone((prevValue) => {
+            return !prevValue
+        })
+    }
 
     return (
-        <div className="task-item" style={{ textDecoration: isDone ? "line-through" : "none"}}>
-            <h4>{task.title}</h4>
-            <p>{task.description}</p>
+        <div className="task-item" onClick={handleClick}>
+            <li style={{ textDecoration: isDone ? "line-through" : "none"}}>
+                <h4>{task.title}</h4>
+                <p>{task.description}</p>
+            </li>
         </div>
     )
 }
